ae9848d9752370dcd5c3f86920baa5285bdc189d
wuzhibo <iamwuzhibo@sina.com>
2017-09-05 15:17:51 +0800

    添加testtag程序

--- shop/db/schema.rb
+++ shop/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828082856) do
+ActiveRecord::Schema.define(version: 20170829053620) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20170828082856) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "variants", force: :cascade do |t|
+    t.integer  "product_id"
+    t.decimal  "price",      precision: 8, scale: 2
+    t.string   "size"
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
   end
 
 end

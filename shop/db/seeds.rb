9dcd92376440d5ee90c8a6b2eacbf921f067a0d7
wuzhibo <iamwuzhibo@sina.com>
2017-09-05 17:31:47 +0800

    添加user用户的认证和调试

--- shop/db/seeds.rb
+++ shop/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: "hi@liwei.me", password: "123456", password_confirmation: "123456")
